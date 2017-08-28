579f0b441705b5b9891b0a347f85724422e2c04f
BVJ <byron@wigroup.co.za>
2017-08-28 19:07:16 +0200

    Initial Commit.

---
+++ src/main/java/co/wigroup/coraline/CoralineApplication.java
@@ -0,0 +1,12 @@
+package co.wigroup.coraline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoralineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoralineApplication.class, args);
+	}
+}
---
+++ src/test/java/co/wigroup/coraline/CoralineApplicationTests.java
@@ -0,0 +1,16 @@
+package co.wigroup.coraline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoralineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
