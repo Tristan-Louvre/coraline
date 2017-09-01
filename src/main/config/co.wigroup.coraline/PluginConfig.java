6d8377edd3488b413f4aada45edf19ae6fabd0c0
BVJ <byron@wigroup.co.za>
2017-09-01 17:07:50 +0200

    Blergh

---
+++ src/main/config/co.wigroup.coraline/PluginConfig.java
@@ -0,0 +1,16 @@
+package co.wigroup.coraline;
+
+import com.eviware.soapui.plugins.PluginAdapter;
+import com.eviware.soapui.plugins.PluginConfiguration;
+
+
+@PluginConfiguration (
+        groupId = "co.wigroup",
+        name = "coraline",
+        version = "1.0.0",
+        description = "A testcase templating plugin for ready!api",
+        infoUrl = "https://github.com/Tristan-Louvre/coraline"
+)
+
+public class PluginConfig extends PluginAdapter {
+}
