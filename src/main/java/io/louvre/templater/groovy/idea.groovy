fd89c86e445d38e7a2584b8a78efb02f7e74ab44
BVJ <byron@wigroup.co.za>
2018-03-02 18:38:24 +0200

    Just adding to the idea, basica planning.

---
+++ src/main/java/io/louvre/templater/groovy/idea.groovy
@@ -0,0 +1,17 @@
+package io.louvre.templater.groovy
+
+class idea {
+
+
+    public hi() {
+
+    }
+
+    // 1: getWadl
+    // 2: search wadl for method names and copy them out into new file
+    // 3: remove extra content
+    // 4: make files for each method: something.xml
+    // 5: update element.order with methodNames.xml
+    // 6: copy paste template
+    // 7: replace {} bracket variables
+}
