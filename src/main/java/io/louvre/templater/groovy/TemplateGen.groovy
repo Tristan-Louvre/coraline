62ded3b2a2486dcebf8ce8eb4873da39b213bd03
BVJ <byron@wigroup.co.za>
2018-03-06 18:38:32 +0200

    Added the skeleton for the entire process.

--- src/main/java/io/louvre/templater/groovy/TemplateGen.groovy
+++ src/main/java/io/louvre/templater/groovy/TemplateGen.groovy
@@ -3,34 +3,74 @@ package io.louvre.templater.groovy
 
 class TemplateGen {
 
-    def static main (String definitionUrl, log) {
-        getFile(definitionUrl, log)
+    def static main (String definitionUrl, String definitionType, log) {
+
+        def fileContents = getFile(definitionUrl, log)
+        def methods = getMethods(definitionType, fileContents, log)
+/*        def sortedMethods = sortMethods(methods)
+        updateElementOrderFile(sortedMethods)
+        createTemplates()*/
+
     }
 
     def static getFile(String definitionUrl, log) {
         String fileContents = new URL(definitionUrl).text
 
-//        log.info(fileContents)
         return fileContents
     }
 
+    def static getMethods(definitionType, String fileContents, log) {
+        //depends on definition file type search for methods in different places
+        //using different parsing strategies [xml,json,text]
 
+        def methods
 
+        switch (definitionType) {
+            case "wadl":
+                def definitionFile = new XmlSlurper().parseText(fileContents)
+                def hello = definitionFile.toString()
+                log.info(hello)
+                break
+            case "wsdl":
+                break
+            case "swagger":
+                break
+            case "openApi":
+                break
+            default:
+                log.info("Definition type not accepted. Valid options are wadl, wsdl, swagger or openApi.")
+                break
+        }
 
+        if (methods != null) {
+            return methods
+        }
 
+    }
 
+    def static sortMethods(methods) {
+        //take in unsorted methods
+        //read in filter criteria from an enum [create, update, delete, gets, get, other]
+        //sort according to enum
 
+        def sortedMethods = 'hi'
 
+        return sortedMethods
+    }
 
-    // 2.0: parse file for method names and store in memory
-    // 3.0: update element.order with methodNames.xml
-        //3.1: order defined with substring with array
-        //3.2: Efficient string sort used to re-allocate
-    // 4.0: loop
-        // 4.1 make file for each method name: something.xml
-        // 4.2: copy paste template into each file
-        // 4.3: replace {} bracket variables
-            // 4.3.1: create uuid for testCase id's ( ${=java.util.UUID.randomUUID()} )
-            // 4.3.2: input variables provided by property file
-            // 4.3.3: input properties hardcoded
+    def static updateElementOrderFile(sortedMethods) {
+        //take in sorted methods
+        //find correct file
+        //add method names and append .xml
+        //return sorted method names with .xml appended
+    }
+
+    def static createTemplates() {
+        //confirm sorted method == full method
+        //loop through array of full method names (.xml) [Ideal world: See if method name file already exists]
+        //create files
+        //copy template over
+        //generate uuid and set all properties with it
+        //set input variables
+    }
 }
